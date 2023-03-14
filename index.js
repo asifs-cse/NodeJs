9674b1d98efe64263ab861428f30908869ebed15
unknown <asifs_cse>
2023-03-14 10:09:35 +0530

    practice

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const _ = require('lodash');
-console.log(_.last([1,2,3,4,5,6,7,1]));
+
+console.log(_.last([1, 2, 3, 4, 5, 6, 7, 1]));
---
+++ practice.js
@@ -0,0 +1,9 @@
+// setTimeout(() => {
+//   console.log('test');
+// }, 1000);
+
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
