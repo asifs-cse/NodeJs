918cbd8cf0f61d7fa01ff56d8a625cf79207c43c
unknown <asifs_cse>
2023-03-16 11:26:18 +0530

    file system module

--- basic/nodeJs/module.js
+++ basic/nodeJs/module.js
@@ -2,15 +2,23 @@
 const path = require('path');
 // os module
 const os = require('os');
+//file system module
+const fs = require('fs');
 
-const myPath = 'D:/Web Development/Node Js/basic/nodeJs/promise.js';
-console.log(path.basename(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
-console.log(path.parse(myPath));
 
-//os
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.cpus());
+// const myPath = 'D:/Web Development/Node Js/basic/nodeJs/promise.js';
+// console.log(path.basename(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
+// console.log(path.parse(myPath));
+
+// //os
+// console.log(os.platform());
+// console.log(os.homedir());
+// console.log(os.freemem());
+// console.log(os.cpus());
+
+//file system module fs
+fs.writeFileSync('myfile.txt', 'Hello programmers');
+const data = fs.readFileSync('myfile.txt');
+console.log(data.toString())
