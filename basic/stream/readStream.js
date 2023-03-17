a9f72156ef17d33fc7c1590236df74e70c39d5ed
unknown <asifs_cse>
2023-03-17 23:50:06 +0530

    read and write stream using pipe

--- basic/stream/readStream.js
+++ basic/stream/readStream.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-const { chunk } = require('lodash');
 
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
-const ourWriteStream = fs.createReadStream(`${__dirname}/output.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk)=>{
-    ourWriteStream.write(chunk);
-})
+// ourReadStream.on('data', (chunk)=>{
+//     ourWriteStream.write(chunk);
+// })
+
+ourReadStream.pipe(ourWriteStream);
